b2118f85ba268548da6f930f2b9587487fe94a1f
Luichang <charlesbarbret@yahoo.com>
2017-09-26 17:05:05 +0200

    completed the first Tutorial Chapter

--- Learning_AS/MynotfirstApp/app/src/main/java/com/example/luichang/my_not_first_app/MainActivity.java
+++ Learning_AS/MynotfirstApp/app/src/main/java/com/example/luichang/my_not_first_app/MainActivity.java
@@ -8,7 +8,8 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    // woriking through the tutorial offered by the android dev page https://developer.android.com/training/index.html
+    public static final String EXTRA_MESSAGE = "com.example.luichang.my_not_first_app.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +18,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendMessage(View view){
+        // using intents to change not only to a new app screen, but also change which java file is currently active,
+        // so new java files are not only used for classes, but can also be used for different screens
         Intent intent = new Intent(this, DisplayMessageActivity.class);
+        // I usually like defining these globally, but it is probably more resource effective to not define them globally
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
+        // intent.putExtra passes arguments to the to be called intent/Activity
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
